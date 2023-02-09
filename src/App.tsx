0443b58b39e593dd3a27bc0b041786674821fbce
Tenith Hasintha <102300949+Tenith01@users.noreply.github.com>
2023-02-09 20:44:18 +0530

    create basics form to get user inputs

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    interface UserInt {
+        name: string,
+        age: string,
+        job: string
+    }
+
+    // const [usersState, setUsersState] = usersState<{ currentUser: UserInt }>({
+    const [usersState, setUsersState] = useState<{ currentUser: UserInt }>({
+        currentUser: {
+            name: "",
+            job: "",
+            age: ""
+        }
+    })
+
+    const onchangeHandler = (e: React.ChangeEvent<HTMLInputElement>): void => {
+        setUsersState({
+            currentUser: {
+                ...usersState.currentUser, [e.target.name]: e.target.value
+            }
+        })
+    }
+    const submitForm = (): void => {
+        console.log("submit")
+    }
+    console.log(usersState.currentUser);
+
+    return (
+        <div className={"container"}>
+            <h1>React with Typescript</h1>
+            <form onSubmit={submitForm}>
+                <label htmlFor={"userName"}> Name : </label>
+                <input
+                    id={"userName"}
+                    type={"text"}
+                    name={"name"}
+                    value={usersState.currentUser.name}
+                    onChange={onchangeHandler}
+                />
+
+                <label htmlFor={"userAge"}> Age : </label>
+                <input
+                    id={"userAge"}
+                    type={"number"}
+                    name={"age"}
+                    value={usersState.currentUser.age}
+                    onChange={onchangeHandler}
+                />
+
+                <label htmlFor={"userJob"}> Job : </label>
+                <input
+                    id={"userJob"}
+                    type={"text"}
+                    name={"Job"}
+                    value={usersState.currentUser.job}
+                    onChange={onchangeHandler}
+                />
+
+                <button type={"submit"}>ADD user</button>
+            </form>
+        </div>
+
+    );
 }
 
 export default App;
