97c4e27056ea693ba475586e191643ce43845a59
Tenith Hasintha <102300949+Tenith01@users.noreply.github.com>
2023-02-09 23:38:20 +0530

    implement user input data adding functions

--- src/App.tsx
+++ src/App.tsx
@@ -8,25 +8,58 @@ function App() {
         job: string
     }
 
+    interface AllUsersInt {
+        currentUser: UserInt;
+        allUsers: Array<UserInt>;
+    }
+
     // const [usersState, setUsersState] = usersState<{ currentUser: UserInt }>({
-    const [usersState, setUsersState] = useState<{ currentUser: UserInt }>({
+    const [usersState, setUsersState] = useState<AllUsersInt>({
         currentUser: {
             name: "",
+            age: "",
             job: "",
-            age: ""
-        }
+        },
+        allUsers: []
     })
 
     const onchangeHandler = (e: React.ChangeEvent<HTMLInputElement>): void => {
         setUsersState({
+            ...usersState,
             currentUser: {
-                ...usersState.currentUser, [e.target.name]: e.target.value
+                ...usersState.currentUser,
+                [e.target.name]: e.target.value
             }
         })
     }
-    const submitForm = (): void => {
-        console.log("submit")
+    const submitForm = (e: React.SyntheticEvent): void => {
+        e.preventDefault();
+
+        setUsersState({
+            currentUser: {
+                name: "",
+                age: "",
+                job: ""
+            },
+            allUsers: [
+                ...usersState.allUsers,
+                usersState.currentUser
+            ]
+        })
     }
+
+    const deleteHandler = () => {
+
+    }
+
+    const allUsers = usersState.allUsers.map((user, i) =>
+        <div key={i}>
+            <h2>{user.name}</h2>
+            <h2>{user.age}</h2>
+            <h2>{user.job}</h2>
+            <button onClick={deleteHandler}>Delete User</button>
+        </div>
+    );
     console.log(usersState.currentUser);
 
     return (
@@ -55,13 +88,14 @@ function App() {
                 <input
                     id={"userJob"}
                     type={"text"}
-                    name={"Job"}
+                    name={"job"}
                     value={usersState.currentUser.job}
                     onChange={onchangeHandler}
                 />
 
                 <button type={"submit"}>ADD user</button>
             </form>
+            {allUsers}
         </div>
 
     );
